26c4de3fdd377647650f53d4c38fe02462dfa907
Ricado Diaz <ricardo_diaz85@hotmail.com>
2024-12-19 13:02:31 -0500

    Implement ContactPicker.js

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,21 @@
+// Import React for building the component
 import React from "react";
 
-export const ContactPicker = () => {
+// Stateless ContactPicker component
+const ContactPicker = ({ contacts, onChange, value, name }) => {
   return (
-    <></>
+    <select onChange={onChange} value={value} name={name}>
+      {/* Default option */}
+      <option value="">No Contact Selected</option>
+
+      {/* Map through contacts and render an option for each contact */}
+      {contacts.map((contact, index) => (
+        <option key={index} value={contact.name}>
+          {contact.name}
+        </option>
+      ))}
+    </select>
   );
 };
+
+export default ContactPicker;
