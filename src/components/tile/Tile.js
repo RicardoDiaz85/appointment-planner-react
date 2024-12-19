617d472ee0f22a919810230fe1bae9472f018ca2
Ricado Diaz <ricardo_diaz85@hotmail.com>
2024-12-19 13:14:28 -0500

    Implement changes for component default exports and imports

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
+// Tile.js
 import React from "react";
 
-export const Tile = () => {
+const Tile = ({ name, description }) => {
   return (
-    <div className="tile-container">
-      
+    <div>
+      <p className="tile-title">{name}</p>
+      {Object.values(description).map((value, index) => (
+        <p key={index} className="tile">{value}</p>
+      ))}
     </div>
   );
 };
+
+export default Tile; // Ensure Tile is exported as default
